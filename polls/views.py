69dc5f8c405c40c6240f3942f128bb07c26df8d3
Lele-do-rock <leledeusuba@gmail.com>
2023-06-20 10:59:50 -0300

    Inclui página com as últimas perguntas.

--- polls/views.py
+++ polls/views.py
@@ -14,3 +14,8 @@ def index(request):
 
 def sobre(request):
     return HttpResponse("Olá este é um app de enquete!")
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
