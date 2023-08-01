17faafe20fd4029af5efab15991d2fd61706b2a4
Lele <leledeusuba@gmail.com>
2023-08-01 11:24:02 -0300

    Aplica layout HTML + CSS

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 print()
 
 def index(request):
-    return HttpResponse("Olá... Seja bem vindo à enquete")
+    # return HttpResponse("Olá... Seja bem vindo à enquete")
 
 def sobre(request):
     return HttpResponse("Olá este é um app de enquete!")
